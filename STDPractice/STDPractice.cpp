ff82dc05f654304c26de408da09ec9859629e66d
David <dlongaron@hotmail.com>
2025-01-20 15:36:11 +0100

    first commit

--- STDPractice/STDPractice.cpp
+++ STDPractice/STDPractice.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
 #include <vector>
-
+#include "TestVector.h"
 
 
 int main()
 {
     std::vector<int> test(9);
     test.push_back(3);
+    TestVector<int> test(9);
 
     std::cout << "Hello World!\n";
 }
