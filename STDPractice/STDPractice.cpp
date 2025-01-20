33965fd8b1c28d9738b6c93561609738a5929562
David <dlongaron@hotmail.com>
2025-01-20 18:50:31 +0100

    testing issues

--- STDPractice/STDPractice.cpp
+++ STDPractice/STDPractice.cpp
@@ -8,8 +8,14 @@ int main()
 {
     std::vector<int> test(9);
     test.push_back(3);
-    TestVector<int> test(9);
+    TestVector<int> test2(9);
+    int val1 = 10, val2 = 20, val3 = 30;
+    test2.pushBack(val1);
+    test2.pushBack(val2);
+    test2.pushBack(val3);
 
-    std::cout << "Hello World!\n";
+    std::cout << test2.at(0) << std::endl; // Debería imprimir 10
+    std::cout << test2.at(1) << std::endl; // Debería imprimir 20
+    std::cout << test2.at(2) << std::endl; // Debería imprimir 30
+
 }
-
--- STDPractice/TestVector.h
+++ STDPractice/TestVector.h
@@ -1,12 +1,15 @@
 #pragma once
+#include <cassert>
+#include <iostream>
 template <typename T>
+
 class TestVector
 {
 public:
     TestVector(int size);
     ~TestVector();
-    void pushBack(T& newElement);
-    T& at(int position);
+    void pushBack(const T& newElement);
+    const T& at(int position);
 
 private:
 
@@ -17,3 +20,48 @@ private:
 };
 
 
+template<typename T>
+TestVector<T>::TestVector(int size) : mVectorSize{ size }, mVectorCapacity{size}
+{
+	assert(size > 0);
+	mVect = new T[size];
+}
+
+template<typename T>
+
+TestVector<T>::~TestVector()
+{
+	delete[] mVect;
+}
+
+template<typename T>
+void TestVector<T>::pushBack(const T& newElement)
+{
+	if (mVectorSize == mVectorCapacity)
+	{
+		mVectorCapacity *= 2;
+		T* temporalVect = new T[mVectorCapacity];
+		for (size_t i = 0; i < mVectorSize - 1; i++)
+		{
+			temporalVect[i] = mVect[i];
+		}
+		delete[] mVect;
+		mVect = temporalVect;
+		
+	}
+		mVect[mVectorSize] = newElement;
+		std::cout << "\n" <<"pushback"<< "\n";
+		++mVectorSize;
+		std::cout << mVect[mVectorSize];
+}
+
+template<typename T>
+
+const T& TestVector<T>::at(int position)
+{
+	std::cout << "\n";
+	std::cout << mVect[0] << "\n";
+	assert(mVectorSize > 0 && (position - 1) < mVectorSize);
+	return mVect[position-1];
+
+}
